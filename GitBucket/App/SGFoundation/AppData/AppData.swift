59587b8bf75e311166888f8794d97762bf21ae6b
sulirong <15528290768@163.com>
2018-01-29 19:35:14 +0800

    添加登录相关视图控制器，添加AppData

---
+++ GitBucket/App/SGFoundation/AppData/AppData.swift
@@ -0,0 +1,22 @@
+//
+//  AppData.swift
+//  GitBucket
+//
+//  Created by sulirong on 2018/1/29.
+//  Copyright © 2018年 CleanAirGames. All rights reserved.
+//
+
+import UIKit
+
+class AppData: NSSecureCoding {
+    static var supportsSecureCoding: Bool {
+        return true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+    }
+    
+    func encode(with aCoder: NSCoder) {
+    }
+}
